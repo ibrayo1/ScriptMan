b7b64918c8a1c20f2c1295fa314d21eb8761c072
Paul Morgan III <pmorgan3@gmail.com>
2019-04-15 21:10:07 -0500

    made the server run

--- server.js
+++ server.js
@@ -5,3 +5,12 @@ let socketIO = require('socket.io')
 let app = express()
 let server = http.Server(app)
 let io = socketIO(server)
+var players = {}
+
+app.use(express.static(__dirname + '/lib'))
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html')
+})
+server.listen(8080, ()=>{
+    console.log(`Listening on ${server.address().port}`)
+})
