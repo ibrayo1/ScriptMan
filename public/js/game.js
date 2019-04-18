173e0548d28cd4ac883984973ef9a3ccd9957d37
Paul Morgan III <pmorgan3@gmail.com>
2019-04-18 18:18:47 -0500

    added ibrahim's changes

---
+++ public/js/game.js
@@ -0,0 +1,163 @@
+var config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 448,
+    height: 496,
+    physics: {
+      default: 'arcade',
+      arcade: {
+        debug: false,
+        gravity: { y: 0 }
+      }
+    },
+    scene: {
+      preload: preload,
+      create: create,
+      update: update
+    } 
+};
+   
+var game = new Phaser.Game(config);
+
+function preload() {
+
+  // graphics (C)opyright Namco
+  this.load.image('dot', 'assets/dot.png');
+  this.load.image('tiles', 'assets/pacman-tiles.png');
+  this.load.spritesheet('pacman', 'assets/pacman.png', {frameWidth: 32, frameHeight: 32});
+  this.load.tilemapTiledJSON('map', 'assets/pacman-map.json');
+}
+
+function create() {
+  // renders the map
+  const map = this.make.tilemap({key: 'map'});
+  const tileset = map.addTilesetImage("pacman-tiles", "tiles");
+  const worldMap = map.createDynamicLayer("Pacman", tileset, 0, 0);
+
+  // set collisions where player collides with everything except dots and open spaces
+  // 7 maps to dots and 14 maps to open spaces
+  worldMap.setCollisionByExclusion([7,14]);
+
+  // create the pacman munch animation
+  this.anims.create({
+    key: 'munch',
+    frames: this.anims.generateFrameNumbers('pacman', {start: 0, end: 2}),
+    frameRate: 15,
+    repeat: -1
+  });
+
+  var self = this;
+  this.socket = io();
+  this.otherPlayers = this.physics.add.group();
+  
+  // listen for current players
+  this.socket.on('currentPlayers', function (players) {
+    Object.keys(players).forEach(function (id) {
+      if (players[id].playerId === self.socket.id) {
+        addPlayer(self, players[id], worldMap);
+      } else {
+        addOtherPlayers(self, players[id], worldMap);
+      }
+    });
+  });
+  this.socket.on('newPlayer', function (playerInfo) {
+    addOtherPlayers(self, playerInfo, worldMap);
+  });
+  this.socket.on('disconnect', function (playerId) {
+    self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+      if (playerId === otherPlayer.playerId) {
+        otherPlayer.destroy();
+      }
+    });
+  });
+
+  //listen for player movements
+  this.socket.on('playerMoved', function (playerInfo) {
+    self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+      if (playerInfo.playerId === otherPlayer.playerId) {
+        otherPlayer.setRotation(playerInfo.rotation);
+        otherPlayer.setPosition(playerInfo.x, playerInfo.y);
+      }
+    });
+  });
+
+  /*
+  // adds player to the center of the world map
+  player = this.physics.add.sprite((14 * 16) + 8, (17 * 16) + 8, 'pacman', 0);
+  player.setSize(16, 16);
+  player.setCollideWorldBounds(true);
+
+  // create the pacman munch animation
+  this.anims.create({
+    key: 'munch',
+    frames: this.anims.generateFrameNumbers('pacman', {start: 0, end: 2}),
+    frameRate: 15,
+    repeat: -1
+  });
+
+  // play the animation
+  player.play('munch');
+  */
+
+  // define cursors as standard arrow keys
+  cursors = this.input.keyboard.createCursorKeys();
+
+  //this.physics.add.collider(player, worldMap);
+
+}
+
+function update(time, delta) {
+  if(this.pacman){
+    // Horizontal movement
+    if (cursors.left.isDown) {
+      this.pacman.body.setVelocityX(-150);
+    } else if (cursors.right.isDown) {
+      this.pacman.body.setVelocityX(150);
+    }
+
+    // Vertical movement
+    if (cursors.up.isDown) {
+      this.pacman.body.setVelocityY(-150);
+    } else if (cursors.down.isDown) {
+      this.pacman.body.setVelocityY(150);
+    }
+
+    // emit player movement
+    var x = this.pacman.x;
+    var y = this.pacman.y;
+    if (this.pacman.oldPosition && (x !== this.pacman.oldPosition.x || y !== this.pacman.oldPosition.y)) {
+      this.socket.emit('playerMovement', { x: this.pacman.x, y: this.pacman.y });
+    }
+    
+    // save old position data
+    this.pacman.oldPosition = {
+      x: this.pacman.x,
+      y: this.pacman.y
+    };
+  }
+}
+
+function addPlayer(self, playerInfo, worldMap) {
+  self.pacman = self.physics.add.sprite(playerInfo.x, playerInfo.y, 'pacman');
+  self.pacman.setSize(16, 16);
+  self.pacman.setCollideWorldBounds(true);
+
+  // play the animation
+  self.pacman.play('munch');
+
+  self.physics.add.collider(self.pacman, worldMap);
+}
+
+function addOtherPlayers(self, playerInfo, worldMap) {
+  self.otherPlayer = self.physics.add.sprite(playerInfo.x, playerInfo.y, 'pacman');
+  self.otherPlayer.setSize(16, 16);
+  self.otherPlayer.setCollideWorldBounds(true);
+  
+  // play the animation
+  self.otherPlayer.play('munch');
+
+  self.otherPlayer.playerId = playerInfo.playerId;
+  self.otherPlayers.add(self.otherPlayer);
+
+  self.physics.add.collider(self.otherPlayer, worldMap);
+}
--- server.js
+++ server.js
@@ -1,17 +1,48 @@
 var express = require('express');
-var http = require('http');
-var path = require('path');
-var socketIO = require('socket.io');
 var app = express();
-var server = http.Server(app);
-var io = socketIO(server);
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
 var players = {};
-// not sure if this is correct but oh well
-app.use(express.static(__dirname + '/static/index.html'));
+
+app.use(express.static(__dirname + '/public'));
+ 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + "/static/index.html");
-    console.log("Hi");
+  res.sendFile(__dirname + '/index.html');
 });
-server.listen(8080, function () {
-    console.log("Listening on " + server.address().port);
+
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  // create a new player and add it to our players object
+  players[socket.id] = {
+    rotation: 0,
+    x: (14 * 16) + 8,
+    y: (17 * 16) + 8,
+    playerId: socket.id,
+    team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+  };
+  // send the players object to the new player
+  socket.emit('currentPlayers', players);
+  // update all other players of the new player
+  socket.broadcast.emit('newPlayer', players[socket.id]);
+ 
+  // when a player disconnects, remove them from our players object
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+    // remove this player from our players object
+    delete players[socket.id];
+    // emit a message to all players to remove this player
+    io.emit('disconnect', socket.id);
+  });
+
+  // when a player moves, update the player data
+  socket.on('playerMovement', function (movementData) {
+    players[socket.id].x = movementData.x;
+    players[socket.id].y = movementData.y;
+    // emit a message to all players about the player that moved
+    socket.broadcast.emit('playerMoved', players[socket.id]);
+  });
+});
+
+server.listen(3000, function () {
+  console.log(`Listening on ${server.address().port}`);
 });
