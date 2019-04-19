cb29f745e6f767f499d688778bf0904592c143fa
Paul Morgan III <pmorgan3@gmail.com>
2019-04-18 20:01:06 -0500

    added a test and updated package.json

--- test/account_test.js
+++ test/account_test.js
@@ -1,5 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const account_1 = require("../public/js/account");
+const chai_1 = require("chai");
 require("mocha");
 describe('Create Account', () => {
+    it('should create a valid account', () => {
+        const account1 = new account_1.Account(1, 0, 0, 0, "blue");
+        chai_1.expect(account1.playerId).to.equal(1);
+        chai_1.expect(account1.x).to.equal(0);
+        chai_1.expect(account1.y).to.equal(0);
+        chai_1.expect(account1.rotation).to.equal(0);
+        chai_1.expect(account1.team_color).to.equal("blue");
+    });
 });
--- test/account_test.ts
+++
@@ -1,7 +0,0 @@
-import { Account } from '../public/js/account'
-import { expect } from 'chai'
-import 'mocha'
-
-describe('Create Account', () => {
-    
-})
