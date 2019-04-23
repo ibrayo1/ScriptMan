073fd379a2e6ae10537f8f949299a46c9caa1c7e
Paul Morgan III <pmorgan3@gmail.com>
2019-04-23 01:06:17 -0500

    started trying to do browser stuff but apparently it doesn't work server side so fuck me I guess

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -7,13 +7,22 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express = __importStar(require("express"));
+const express = require("express");
 var app = express();
 const http_1 = require("http");
 let server = new http_1.Server(app);
-const io = __importStar(require("socket.io"));
-var players = {};
+const IO = __importStar(require("socket.io"));
+var playersAccountHash = {};
+var playersPlayerRankHash = {};
 var number_of_sockets = 0;
+var io = IO.listen(server);
+var PlayerNumber;
+(function (PlayerNumber) {
+    PlayerNumber[PlayerNumber["PlayerOne"] = 0] = "PlayerOne";
+    PlayerNumber[PlayerNumber["PlayerTwo"] = 1] = "PlayerTwo";
+    PlayerNumber[PlayerNumber["PlayerThree"] = 2] = "PlayerThree";
+    PlayerNumber[PlayerNumber["PlayerFour"] = 3] = "PlayerFour";
+})(PlayerNumber || (PlayerNumber = {}));
 app.use(express.static('/public'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/lobby.html');
@@ -21,15 +30,20 @@ app.get('/', (req, res) => {
 io.on('connection', function (socket) {
     if (number_of_sockets === 0) {
         document.getElementById('playerOne').className = socket.id;
+        playersPlayerRankHash[socket.id] = PlayerNumber.PlayerOne;
+        document.getElementById('playerOne').innerText = "Player One: Connected";
     }
     else if (number_of_sockets === 1) {
         document.getElementById('playerTwo').className = socket.id;
+        playersPlayerRankHash[socket.id] = PlayerNumber.PlayerTwo;
     }
     else if (number_of_sockets === 2) {
         document.getElementById('playerThree').className = socket.id;
+        playersPlayerRankHash[socket.id] = PlayerNumber.PlayerThree;
     }
     else if (number_of_sockets === 3) {
         document.getElementById('playerFour').className = socket.id;
+        playersPlayerRankHash[socket.id] = PlayerNumber.PlayerFour;
     }
     number_of_sockets += 1;
 });
@@ -41,11 +55,26 @@ let greenBox = document.getElementById("greenBox");
 greenBox.onclick(this.chooseGreen());
 let purpleBox = document.getElementById("purpleBox");
 purpleBox.onclick(this.choosePurple());
+var usernameBox = document.getElementById("usernameBox").nodeValue;
+function usernameChanged(name) {
+    io.emit('username changed', (socket) => {
+    });
+}
+// This will get the number of the player whose socket ID is passed
+// as an argument
+function getPlayerNumber(socketID) {
+    return playersPlayerRankHash[socketID];
+}
+// Functions called if player has chosen the respective color
 function chooseBlue() {
+    // TODO
 }
 function choosePurple() {
+    // TODO
 }
 function chooseGreen() {
+    // TODO
 }
 function chooseYellow() {
+    // TODO
 }
--- public/js/lobby.ts
+++ public/js/lobby.ts
@@ -1,14 +1,24 @@
 import { Account } from "./account"
 import * as s from 'http'
 
-import * as express from 'express'
+import express = require('express')
 var app = express()
 import { Server } from 'http';
 let server = new Server(app);
-import * as io from 'socket.io'
-var players = {};
+import * as IO from 'socket.io'
+import { Socket as socket } from "net";
+var playersAccountHash = {};
+var playersPlayerRankHash = {};
 var number_of_sockets = 0
+var io = IO.listen(server)
 
+
+enum PlayerNumber {
+    PlayerOne,
+    PlayerTwo,
+    PlayerThree,
+    PlayerFour
+}
 
 app.use(express.static('/public'))
 
@@ -19,15 +29,20 @@ app.get('/', (req, res) => {
 io.on('connection', function (socket) {
     if(number_of_sockets === 0){
         document.getElementById('playerOne').className = socket.id
+        playersPlayerRankHash[socket.id] = PlayerNumber.PlayerOne
+        document.getElementById('playerOne').innerText = "Player One: Connected"
     }
     else if(number_of_sockets === 1){
         document.getElementById('playerTwo').className = socket.id
+        playersPlayerRankHash[socket.id] = PlayerNumber.PlayerTwo
     }
     else if(number_of_sockets === 2){
         document.getElementById('playerThree').className = socket.id
+        playersPlayerRankHash[socket.id] = PlayerNumber.PlayerThree
     }
     else if(number_of_sockets === 3){
         document.getElementById('playerFour').className = socket.id
+        playersPlayerRankHash[socket.id] = PlayerNumber.PlayerFour
     }
     number_of_sockets += 1
 })
@@ -40,20 +55,34 @@ let greenBox = document.getElementById("greenBox")
 greenBox.onclick(this.chooseGreen())
 let purpleBox = document.getElementById("purpleBox")
 purpleBox.onclick(this.choosePurple())
+var usernameBox = document.getElementById("usernameBox").nodeValue;
 
+function usernameChanged(name: string){
+    io.emit('username changed', (socket) => {
 
+    })
+
+}
+
+// This will get the number of the player whose socket ID is passed
+// as an argument
+function getPlayerNumber(socketID): PlayerNumber {
+    return playersPlayerRankHash[socketID]
+}
+
+// Functions called if player has chosen the respective color
 function chooseBlue() {
-
+    // TODO
 }
 
 function choosePurple() {
-
+    // TODO
 }
 
 function chooseGreen() {
-
+    // TODO
 }
 
 function chooseYellow() {
-    
+    // TODO
 }
