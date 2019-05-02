8c8232650c9280e5b5fe541d597086a692240c20
Logan <logan.scott441@gmail.com>
2019-05-01 22:44:09 -0500

    Fixed merge issue from ibrahim's branch

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 var express = require('express');
 // This imports the account class from the Account.js file
 const { Account } = require(__dirname + '/public/js/account.js');
@@ -7,8 +6,8 @@ var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 var players = {};
 var dot = {
-  x: 30,
-  y: 30
+  x: 24,
+  y: 24
 };
 
 //We need to synchronize the dot map between servers and clients
@@ -25,97 +24,6 @@ for(var i = 0; i < map.length; i++){
     dotArray.push(1);
   }
 }
-
-app.use(express.static(__dirname + '/public'));
- 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', function (socket) {
-  console.log('a user connected');
-  // create a new player and add it to our players object
-  console.log('their socket.id: ' + socket.id)
-  players[socket.id] = new Account(
-    socket.id, // ID
-    (14 * 16) + 8, // x
-    (17 * 16) + 8, // y
-    0, // rotation
-    0, // score intialized to zero
-    '', // username is intialized to empty string
-  );
- 
-  // sets the name of the player
-  socket.on('playerName', function(nameData){
-    players[socket.id].username = nameData.username;
-    console.log('their name is: ' + players[socket.id].username);
-    io.emit('scoreUpdate', players);
-  });
-
-  // send the players object to the new player
-  socket.emit('currentPlayers', players);
-
-  // send the dot object to new player
-  //socket.emit('dotLocation', dot);
-
-  // send the entire dotmap
-  socket.emit('dotMap', dotArray);
-
-  // update all other players of the new player
-  socket.broadcast.emit('newPlayer', players[socket.id]);
-
-  // send the current scores of all the players
-  socket.emit('scoreUpdate', players);
-
-  // when a player disconnects, remove them from our players object
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
-    // remove this player from our players object
-    delete players[socket.id];
-    // emit a message to all players to remove this player
-    io.emit('disconnect', socket.id);
-  });
-
-  // when a player moves, update the player data
-  socket.on('playerMovement', function (movementData) {
-    players[socket.id].x = movementData.x;
-    players[socket.id].y = movementData.y;
-    players[socket.id].angle = movementData.angle;
-    // emit a message to all players about the player that moved
-    socket.broadcast.emit('playerMoved', players[socket.id]);
-  });
-
-  socket.on('destroyDot' , function(dotIndex){
-      console.log(dotIndex);
-  });
-  socket.on('dotCollected', function (dotLoc) {
-    players[socket.id].score += 10;
-
-    console.log(players[socket.id].score); // for debugging purposes
-    
-    dot.x = dotLoc.x;
-    dot.y = dotLoc.y;
-    io.emit('dotLocation', dot);
-    // emit a message to all player that updated his score
-    io.emit('scoreUpdate', players);
-  });
-
-});
-
-server.listen(3000, function () {
-  console.log(`Listening on ${server.address().port}`);
-/* eslint-disable no-console */
-var express = require('express');
-// This imports the account class from the Account.js file
-const { Account } = require(__dirname + '/public/js/account.js');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io').listen(server);
-var players = {};
-var dot = {
-  x: 24,
-  y: 24
-};
 
 var blackdragon = {x: 24, y: 24, vx: 100, vy: 100};
 var stringe = {x: 150, y: 150, vx: -100, vy: 100};
@@ -147,6 +55,9 @@ io.on('connection', function (socket) {
     console.log('their name is: ' + players[socket.id].username);
     io.emit('scoreUpdate', players);
   });
+
+  // send the entire dotmap
+  socket.emit('dotMap', dotArray);
 
   // send the players object to the new player
   socket.emit('currentPlayers', players);
@@ -236,9 +147,8 @@ io.on('connection', function (socket) {
     jrreaper.vy = jrreaperLoc.vy;
     socket.broadcast.emit('jrreaperMoved', jrreaper);
   });
-
 });
 
 server.listen(3000, function () {
-  console.log(`Listening on ${server.address().port}`);
+  console.log("Listening on port 3000");
 });
